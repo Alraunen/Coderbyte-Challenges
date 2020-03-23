cadd68378ba91ecc0f04d7a08e5f821aeace0ff8
Alraunen <30340428+Alraunen@users.noreply.github.com>
2020-03-23 02:27:02 +0100

    Two challenges completed

---
+++ Alraunen.BinaryReversal/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Alraunen.BinaryReversal
+{
+    class MainClass
+    {
+        /// <summary>
+        /// Returns any positive integer number as its binary inversion, padded to the next highest n*8 bits in length.
+        /// </summary>
+        /// <param name="str">The integer number to reverse.</param>
+        /// <returns>The reversed number.</returns>
+        /// <example>Input: 120 (0x0111 1000)
+        /// Output: 30 (0x0001 1110)
+        /// 
+        /// Input: 6014 (0x0110 0000 0001 0100)
+        /// Output: 10246 (0x0010 1000 0000 0110)</example>
+        public static string BinaryReversal(string str)
+        {
+            var val = uint.Parse(str);
+            uint result = 0;
+            while (val != 0)
+            {
+                for (var i = 0; i < 8; i++)
+                {
+                    result = result << 1;
+                    result = result | (val & 1);
+                    val = val >> 1;
+                }
+            }
+            return result.ToString();
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter number to reverse: ");
+            var input = Console.ReadLine();
+            var output = BinaryReversal(input);
+            var textLength = Math.Max(input.Length, output.Length);
+            Console.WriteLine("Input:  {0,"+textLength+"}, 0x{1}\n" +
+                              "Output: {2,"+textLength+"}, 0x{3}", input, formatBinaryNumber(input), output, formatBinaryNumber(output));
+            Console.ReadKey();
+        }
+
+        private static string formatBinaryNumber(string input)
+        {
+            var result = Convert.ToString(Convert.ToUInt32(input), 2);
+            result = result.PadLeft(result.Length + (8 - result.Length % 8) % 8, '0');
+            result = Regex.Replace(result, ".{4}", "$0 ").TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ FindIntersection/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindIntersection
+{
+    class Program
+    {
+        public static string FindIntersection(string[] strArr) // using LINQ
+        {
+            try
+            {
+                return converter(strArr[0]).Intersect(converter(strArr[1]))
+                    .Select(item => item.ToString())
+                    .Aggregate((agg, item) => agg + ',' + item);
+            }
+            catch
+            {
+                return "false";
+            }
+        }
+
+        public static string findIntersection(string[] strArr) // using enumerators
+        {
+            var arr1Ptr = converter(strArr[0]).GetEnumerator();
+            var arr2Ptr = converter(strArr[1]).GetEnumerator();
+            if (!arr1Ptr.MoveNext() || !arr2Ptr.MoveNext())
+                return "false";
+            
+            IEnumerator<int> tmp;
+            var result = "";
+            Action swap = () => { tmp = arr2Ptr; arr2Ptr = arr1Ptr; arr1Ptr = tmp; };
+
+            if (arr1Ptr.Current > arr2Ptr.Current)
+                swap();
+
+            do
+            {
+                if (arr1Ptr.Current == arr2Ptr.Current)
+                {
+                    result += arr1Ptr.Current + ",";
+                    arr2Ptr.MoveNext();
+                }
+                else if (arr1Ptr.Current > arr2Ptr.Current)
+                    swap();
+            } while (arr1Ptr.MoveNext());
+
+            if (result == "")
+                return "false";
+            result = result.Remove(result.Length - 1);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findIntersection(new string[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" }));
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<int> converter(string str) => str.Split(',').Select(item => int.Parse(item.Trim()));
+    }
+}
